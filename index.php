504ab6d0da2ccc36ab7cc4304fb22efb26158ddc
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-01-23 17:15:19 -0300

    site do doug

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
     <link rel="stylesheet" href="CSS/estilo.css">
     <title>Document</title>
 </head>
-<body>
+<body class="fundo">
     
     <?php include('header.html')?>
 
+    <div class="container">
+
+        <div class="energiasol">
+        <img src="images/energia_solar.png" alt="energia solar" class="imagem_energia">
+        <p class="padding font_tamanho">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+    </div>
 </body>
 </html>
