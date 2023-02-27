f3aed7f60b002ad43f4154d96f0c5bb375182bf6
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-02-27 11:59:17 -0300

    banco de dados do site

---
+++ banco.php
@@ -0,0 +1,173 @@
+<?php
+$dados = array(
+    [
+    "id" => 1,
+    "nome" => "Ar Condicionado Elgin",
+    "descricao" => "Eco Plus Geração Ii 12.000 F Unidade Interna - Branco, 9,4kg, O modelo clean e elegante é adequado a qualquer ambiente. Toda a linha trabalha com o gás ecológico R-410A, possui selo Procel (classificação A no INMETRO). A unidade interna possui um display invisível indicando a temperatura do ambiente, que só acende ao ligar a unidade.",
+    "preco" => 1.091,00,
+    "imagem" => 'images/arcondicionado_elgin.jpg'
+    ],
+    
+    [
+    "id" => 2,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 3,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 3,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+    
+    [
+    "id" => 4,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    
+    [
+    "id" => 5,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 6,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 7,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 8,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+     "imagem" => ''
+    ],
+
+    [
+    "id" => 9,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 10,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 11,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 12,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 13,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 14,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 15,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 16,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 17,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 18,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+                        
+    [
+    "id" => 19,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+    [
+    "id" => 20,
+    "nome" => "",
+    "descricao" => "",
+    "preco" => 1000.99,
+    "imagem" => ''
+    ],
+
+);
+?>
